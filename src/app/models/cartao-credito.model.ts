57adcbec22aa9910f294b01f659ede74f3370112
Carlos Rocha <60583506+toltecas@users.noreply.github.com>
2026-04-06 15:39:25 -0300

    1) Aplicando camelCase (Clean Code) para padronizacao dos elementos do template.

--- src/app/models/cartao-credito.model.ts
+++ src/app/models/cartao-credito.model.ts
@@ -11,7 +11,7 @@ export interface MembrosFamilia {
  * Representam as Movimentacoes (Transacoes)
  */
 export interface MovimentosCartao {
-  id?: number;         ///Opcional gerado via Pattern Generate
+  codigoMovimento?: number;         ///Opcional gerado via Pattern Generate
   codigoMembroFamilia: number;    //FK link para MembroFamilia
   valorMovimento: number;
   dataMovimento: string | Date; // ISO string format (YYYY-MM-DD) from Spring
